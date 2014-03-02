86732f5eaf19677ac77f3317c6dc29aae2c1f64c
lyase damasked <lyase.d@gmail.com>
2014-03-02 09:16:31 +0100

    using sqlite as database

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 #include <vidanueva/app/App.hpp>
 #include <vidanueva/auth/Services.hpp>
 #include <vidanueva/postgres.hpp>
-
+#include <Wt/Dbo/backend/Sqlite3>
 int main(int argc, char** argv) {
-    Wt::Dbo::backend::Postgres pg(pg_string);
+//    Wt::Dbo::backend::Postgres pg(pg_string);
+    Wt::Dbo::backend::Sqlite3 sql("appRoot()+myapp.db");
     const Auth::Services authServices;
-    Wt::WRun(argc, argv, [&](const Wt::WEnvironment& env) { return new vidanueva::App(env, pg, authServices); });
+    Wt::WRun(argc, argv, [&](const Wt::WEnvironment& env) { return new vidanueva::App(env, sql, authServices); });
 }
